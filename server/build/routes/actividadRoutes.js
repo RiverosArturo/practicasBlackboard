69c1b3189441500311a3b9c3b0cefdd67b36c325
RiverosArturo <riveros45arturo@gmail.com>
2022-07-30 23:27:55 -0500

    d

--- server/build/routes/actividadRoutes.js
+++ server/build/routes/actividadRoutes.js
@@ -7,7 +7,7 @@ const express_1 = require("express");
 const actividadController_1 = __importDefault(require("../controllers/actividadController"));
 class actividadRoutes {
     constructor() {
-        this.router = express_1.Router();
+        this.router = (0, express_1.Router)();
         this.config();
     }
     config() {
--- server/build/routes/equiporoutes.js
+++ server/build/routes/equiporoutes.js
@@ -7,7 +7,7 @@ const express_1 = require("express");
 const equipoController_1 = __importDefault(require("../controllers/equipoController"));
 class EquipoRoutes {
     constructor() {
-        this.router = express_1.Router();
+        this.router = (0, express_1.Router)();
         this.config();
     }
     config() {
