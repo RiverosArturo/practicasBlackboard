f64eb34dfe5cffd108e5276d9b59179f83cc6d1e
RiverosArturo <riveros45arturo@gmail.com>
2022-08-09 23:20:13 -0500

    nnn

--- server/build/routes/equipoStudentsRoutes.js
+++ server/build/routes/equipoStudentsRoutes.js
@@ -13,12 +13,12 @@ class EquipoStudentsRoutes {
     config() {
         this.router.get('/', equipoStudentsController_1.default.getEquipos);
         this.router.get('/:id', equipoStudentsController_1.default.getOneEquipo);
-        //this.router.get('/:nTrabajador/:nrc/:id', equipoStudentsController.getEquiposBien);
+        this.router.get('/:nTrabajador/:nrc/:id', equipoStudentsController_1.default.getEquiposBien);
         this.router.post('/', equipoStudentsController_1.default.saveEquipo);
         this.router.put('/:id', equipoStudentsController_1.default.updateEquipo);
         //------------------------------------------------------------------------------------        
         this.router.post('/', equipoStudentsController_1.default.saveStudentEquipo);
-        this.router.get('/:id_equipo/:nrc/:nTrabajador', equipoStudentsController_1.default.get1Equipo);
+        this.router.get('/:id_equipo', equipoStudentsController_1.default.get1Equipo);
         this.router.delete('/:matricula/:id_equipo/:nrc/:nTrabajador', equipoStudentsController_1.default.deleteStudentEquipo);
         this.router.delete('/:id_equipo/:nrc/:nTrabajador', equipoStudentsController_1.default.deleteStudentsEquipo);
         this.router.delete('/:id/:nombre/:curso_nrc/:nTrabajador', equipoStudentsController_1.default.deleteEquipo);
--- server/src/routes/equipoStudentsRoutes.ts
+++ server/src/routes/equipoStudentsRoutes.ts
@@ -14,13 +14,13 @@ class EquipoStudentsRoutes {
 
         this.router.get('/', equipoStudentsController.getEquipos);
         this.router.get('/:id', equipoStudentsController.getOneEquipo);
-        //this.router.get('/:nTrabajador/:nrc/:id', equipoStudentsController.getEquiposBien);
+        this.router.get('/:nTrabajador/:nrc/:id', equipoStudentsController.getEquiposBien);
         this.router.post('/', equipoStudentsController.saveEquipo);
         this.router.put('/:id', equipoStudentsController.updateEquipo); 
         
 //------------------------------------------------------------------------------------        
         this.router.post('/', equipoStudentsController.saveStudentEquipo);
-        this.router.get('/:id_equipo/:nrc/:nTrabajador', equipoStudentsController.get1Equipo);
+        this.router.get('/:id_equipo', equipoStudentsController.get1Equipo);
         this.router.delete('/:matricula/:id_equipo/:nrc/:nTrabajador', equipoStudentsController.deleteStudentEquipo);
         this.router.delete('/:id_equipo/:nrc/:nTrabajador', equipoStudentsController.deleteStudentsEquipo);
         this.router.delete('/:id/:nombre/:curso_nrc/:nTrabajador', equipoStudentsController.deleteEquipo); 
