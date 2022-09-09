1db8686601724a243bba62bfc3d845925aebb445
tonyslayer <98005162+tonyslayer@users.noreply.github.com>
2022-09-09 01:22:13 -0500

    a

--- ClientGestion/src/app/page/home-stud/estudiante/perfil/perfil.page.ts
+++ ClientGestion/src/app/page/home-stud/estudiante/perfil/perfil.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MenuController } from '@ionic/angular';
+import { DatosService } from 'src/app/services/datos.service';
+
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  user:string;
+  nrc:number;
+  nTrabajador:number;
+  curso:string;
+
+  constructor( private menu:MenuController, private datosService: DatosService,private router: Router, private activedRoute:ActivatedRoute ) { }
 
   ngOnInit() {
+    const params = this.activedRoute.snapshot.params;     
+    this.user = params.user;  
+    this.nrc = params.nrc;
+    this.user = params.user;
+    this.nrc = params.nrc;
+    this.nTrabajador = params.nTrabajador;
+  }
+
+  //---------------------------------------------------------------------------------
+  OpenMenuStud(){
+    this.menu.enable(true,'MenuStud');
+    this.menu.open('MenuStud');
+  }
+  openmenu0(){
+    this.menu.enable(true,'end');
+    this.menu.open('end')
   }
 
 }
