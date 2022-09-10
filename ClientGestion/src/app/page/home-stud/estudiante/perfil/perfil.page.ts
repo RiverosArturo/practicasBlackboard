ab8a1b1bf86e8b3fb0afa77fdf7921d0a7cec70e
tonyslayer <98005162+tonyslayer@users.noreply.github.com>
2022-09-10 01:20:26 -0500

    perfil

--- ClientGestion/src/app/page/home-stud/estudiante/perfil/perfil.page.ts
+++ ClientGestion/src/app/page/home-stud/estudiante/perfil/perfil.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { DatosService } from 'src/app/services/datos.service';
+import { Student } from 'src/app/models/Student';
+
+
 
 
 @Component({
@@ -12,9 +15,20 @@ import { DatosService } from 'src/app/services/datos.service';
 export class PerfilPage implements OnInit {
 
   user:string;
+  matricula:number;
   nrc:number;
   nTrabajador:number;
   curso:string;
+  
+  perfil:boolean = false;
+  EditPerfil:boolean = false;
+  EditPassword:boolean = false;
+
+  student: Student = {
+    password: '',
+  };
+  students:any = [];
+
 
   constructor( private menu:MenuController, private datosService: DatosService,private router: Router, private activedRoute:ActivatedRoute ) { }
 
@@ -25,8 +39,54 @@ export class PerfilPage implements OnInit {
     this.user = params.user;
     this.nrc = params.nrc;
     this.nTrabajador = params.nTrabajador;
+    this.matricula = params.user;
+    this.getAlumno(this.matricula);
+    this.perfil = true;
+  }
+  //-------------------------------- Funciones -----------------------------------
+  
+  getAlumno(matricula:number){
+    this.datosService.getStudent(matricula)
+      .subscribe(
+        res =>{          
+          this.student = res;        
+        },
+        err => console.error(err)
+      )
+  }
+  updateStudent(){
+    this.datosService.updateStudent(this.student.matricula, this.student)
+    .subscribe(
+      res =>{
+        console.log(res);
+        this.getStudent();        
+      },
+      err => console.error(err)
+    )
+  }
+  getStudent(){
+    this.datosService.getStudents().subscribe(
+      res => {
+        this.students = res;     
+      },
+      err => console.error(err)
+    );
   }
 
+  editPerfil(){
+    this.EditPerfil = true;    
+    this.EditPassword = false;
+    this.perfil = false;
+  }
+  editPassword(){
+    this.EditPassword = true;
+    this.EditPerfil = false;
+    this.perfil = false;
+  }
+  close(){
+    this.EditPerfil = false;
+    this.EditPassword = false;
+  }
   //---------------------------------------------------------------------------------
   OpenMenuStud(){
     this.menu.enable(true,'MenuStud');
